1ed111b4e90a4e54ca2abf0e47adc6e0ad67b753
Gregory Petroski <GregoryPetroski@eharmony.com>
2016-02-18 10:01:16 -0800

    Initial commit importing files

---
+++ README.md
@@ -0,0 +1,2 @@
+# eh-swagger-validator
+A fork of swagger-validator as a jar instead of a war so it can be embedded in eh-swagger-repository
---
+++ src/main/java/io/swagger/validator/models/Instance.java
@@ -0,0 +1,13 @@
+package io.swagger.validator.models;
+
+public class Instance {
+    private String pointer;
+
+    public String getPointer() {
+        return pointer;
+    }
+
+    public void setPointer(String pointer) {
+        this.pointer = pointer;
+    }
+}
